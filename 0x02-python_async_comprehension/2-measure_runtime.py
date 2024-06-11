4fdf8ea92f70adf2648d5be5406cf720585b57aa
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-11 14:27:59 +0000

    measure time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module to measure runtime of paallel comprehensions """
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ measure runtime of an async comprehension"""
+    start = time.perf_counter()
+    task = async_comprehension()
+    end = time.perf_counter()
+    timeTaken = end - start
+    return timeTaken
